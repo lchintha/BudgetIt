960ebc7d52f3244a65b5509b113beb49766bcfa6
thottempudi srikar <camera_04kappa@icloud.com>
2024-11-15 11:45:54 -0600

    optimizing imports

--- composeApp/src/commonMain/kotlin/com/iquad/budgetit/App.kt
+++ composeApp/src/commonMain/kotlin/com/iquad/budgetit/App.kt
@@ -3,13 +3,32 @@ package com.iquad.budgetit
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.iquad.budgetit.presentation.budget.BudgetSetupScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
+
+sealed class Screen {
+    data object BudgetSetup : Screen()
+    data object Home : Screen() // Next screen after budget setup
+}
 
 @Composable
 @Preview
 fun App() {
+    var currentScreen by remember { mutableStateOf<Screen>(Screen.BudgetSetup) }
+
     MaterialTheme {
-        BudgetSetupScreen()
+        when (currentScreen) {
+            Screen.BudgetSetup -> BudgetSetupScreen(
+                onNavigateNext = { currentScreen = Screen.Home }
+            )
+
+            Screen.Home -> {
+                // Home screen implementation
+            }
+        }
     }
 }
