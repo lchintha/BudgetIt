14f452c7a00ba72c2e6b770da2634507de1f3edb
thottempudi srikar <camera_04kappa@icloud.com>
2024-11-14 22:39:07 -0600

    adding budget it theme

---
+++ composeApp/src/commonMain/kotlin/com/iquad/budgetit/theme/Color.kt
@@ -0,0 +1,7 @@
+import androidx.compose.ui.graphics.Color
+
+val Blue = Color(0xFF2196F3)
+val DarkBlue = Color(0xFF1976D2)
+val LightBlue = Color(0xFF64B5F6)
+val Orange = Color(0xFFFF9800)
+val LightOrange = Color(0xFFFFB74D)
---
+++ composeApp/src/commonMain/kotlin/com/iquad/budgetit/theme/Theme.kt
@@ -0,0 +1,31 @@
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+import androidx.compose.material.darkColors
+import androidx.compose.runtime.Composable
+
+private val LightColors = lightColors(
+    primary = Blue,
+    primaryVariant = DarkBlue,
+    secondary = Orange
+    // Add other color customizations
+)
+
+private val DarkColors = darkColors(
+    primary = LightBlue,
+    primaryVariant = Blue,
+    secondary = LightOrange
+    // Add other color customizations
+)
+
+@Composable
+fun BudgetItTheme(
+    darkTheme: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = if (darkTheme) DarkColors else LightColors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
