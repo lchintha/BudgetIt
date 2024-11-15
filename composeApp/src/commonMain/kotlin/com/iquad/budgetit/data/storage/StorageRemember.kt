f96070923fbfe71c8f2b3c7d898f6e83fdd893e1
thottempudi srikar <camera_04kappa@icloud.com>
2024-11-14 23:58:25 -0600

    fixing comments

--- composeApp/src/commonMain/kotlin/com/iquad/budgetit/data/storage/StorageRemember.kt
+++ composeApp/src/commonMain/kotlin/com/iquad/budgetit/data/storage/StorageRemember.kt
@@ -3,7 +3,7 @@ package com.iquad.budgetit.data.storage
 import androidx.compose.runtime.Composable
 
 /**
- * This file provides a common interface for creating platform-specific com.iquad.budgetit.data.storage.Storage instances
+ * This file provides a common interface for creating platform-specific Storage instances
  * in a Compose-friendly way. The actual implementations are provided in the platform-specific
  * source sets.
  */
