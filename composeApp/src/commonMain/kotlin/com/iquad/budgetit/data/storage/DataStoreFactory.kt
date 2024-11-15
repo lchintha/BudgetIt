144f4415e4f0c8d68b63813345f4babcfbd6078f
thottempudi srikar <camera_04kappa@icloud.com>
2024-11-15 13:59:47 -0600

    removing duplicates

--- composeApp/src/commonMain/kotlin/com/iquad/budgetit/data/storage/DataStoreFactory.kt
+++
@@ -1,6 +0,0 @@
-package com.iquad.budgetit.data.storage
-
-import androidx.datastore.core.DataStore
-import com.iquad.budgetit.proto.BudgetPreferencesKt
-
-expect fun createDataStore(context: Any): DataStore<BudgetPreferencesKt>
