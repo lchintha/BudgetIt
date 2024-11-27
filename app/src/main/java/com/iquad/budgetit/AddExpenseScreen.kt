cfec66973cb8936172fd4428473b218e0731fa42
Praveen <chintap94@gmail.com>
2024-11-26 22:44:22 -0600

    Work is in progress to create a custom amount input field.

--- app/src/main/java/com/iquad/budgetit/AddExpenseScreen.kt
+++ app/src/main/java/com/iquad/budgetit/AddExpenseScreen.kt
@@ -1,14 +1,18 @@
 package com.iquad.budgetit
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.iquad.budgetit.common.BudgetItToolBar
+import com.iquad.budgetit.common.InputAmountTextField
 
 @Composable
 fun AddExpenseScreen(navController: NavController){
@@ -21,12 +25,11 @@ fun AddExpenseScreen(navController: NavController){
             title = stringResource(R.string.add_expense),
             toolbarOption = stringResource(R.string.save)
         )
+        Spacer(modifier = Modifier.height(8.dp))
+        InputAmountTextField(
+            onValueChange = {}
+        )
     }
-}
-
-@Composable
-fun EnterAmountField() {
-
 }
 
 @Preview
---
+++ app/src/main/java/com/iquad/budgetit/common/InputAmountTextField.kt
@@ -0,0 +1,74 @@
+package com.iquad.budgetit.common
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun InputAmountTextField(
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var amount by remember { mutableStateOf("") }
+
+    TextField(
+        modifier = modifier.fillMaxWidth(),
+        value = amount,
+        onValueChange = { input ->
+            if (input.matches(Regex("^\\d*\\.?\\d*\$"))) {
+                amount = input
+                onValueChange(input)
+            }
+        },
+        placeholder = {
+            Text(
+                text = "$0",
+                modifier = Modifier.fillMaxWidth(),
+                color = Color.Black,
+                fontSize = 36.sp,
+                textAlign = TextAlign.Center
+            )
+        },
+        textStyle = TextStyle(
+            fontSize = 36.sp,
+            textAlign = TextAlign.Center,
+
+        ),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Decimal,
+            imeAction = ImeAction.Done
+        ),
+//        visualTransformation = DollarVisualTransformation(),
+        singleLine = true,
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = Color.Transparent,
+            focusedContainerColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            cursorColor = Color.Transparent
+        ),
+    )
+}
+
+@Preview
+@Composable
+fun InputAmountTextFieldPreview() {
+    InputAmountTextField(
+        onValueChange = {}
+    )
+}
